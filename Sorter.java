b60bb6d0043da516541d9bc35dd8984dd94787f1
khalid <khaled125900@gmail.com>
2023-09-25 23:15:42 +0300

    insertion sort

--- Sorter.java
+++ Sorter.java
@@ -1,7 +1,9 @@
+import java.util.Arrays;
+
 public class Sorter{
-    public static void main(String[] args) {
+    
         
-    }
+    
 
     private static void selectionSort(int[] array){
         for(int i = 0; i < array.length - 1; i++){
@@ -16,5 +18,33 @@ public class Sorter{
                 array[i] = min;
             }
         }
+    }
+     public static void insertionSort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+
+            arr[j + 1] = key;
+        }
+    }
+
+    public static void sort(int[] array) {
+        insertionSort(array);
+    }
+
+    public static void main(String[] args) {
+        int[] myArray = {12, 11, 13, 5, 6};
+        System.out.println("Unsorted array: " + Arrays.toString(myArray));
+
+        sort(myArray);
+
+        System.out.println("Sorted array: " + Arrays.toString(myArray));
     }
 }
